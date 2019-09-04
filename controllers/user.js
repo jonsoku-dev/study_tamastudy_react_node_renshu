046b60c5972902d92f509560886449d0471f9007
rakutenjongseok <whdtjr2792@gmail.com>
2019-09-05 06:22:21 +0900

    validator.... fuck!

--- controllers/user.js
+++ controllers/user.js
@@ -1,10 +1,19 @@
 const User = require("../models/user");
 const { errorHandler } = require("../helpers/dbErrorHandler");
+const { validationResult } = require("express-validator");
 
 exports.signup = async (req, res) => {
+  /* validator */
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+
   const { name, email, password } = req.body;
   try {
     let user = await User.findOne({ email });
+
+    // user check
     if (user) {
       return res.status(400).json({ errors: [{ msg: "user already exists!" }] });
     }
@@ -13,6 +22,8 @@ exports.signup = async (req, res) => {
       email,
       password
     });
+
+    // save user
     await user.save((err, user) => {
       if (err) {
         return res.status(400).json({
--- routes/user.js
+++ routes/user.js
@@ -1,8 +1,30 @@
 const express = require("express");
 const router = express.Router();
+const { check, validationResult } = require("express-validator");
 // controllers
 const { signup } = require("../controllers/user");
+//const { userSignupValidator } = require("../validator");
 
-router.post("/signup", signup);
+router.post(
+  "/signup",
+  [
+    check("name", "Name is required")
+      .not()
+      .isEmpty(),
+    check("email", "Email must be between 3 to 32 characters")
+      .matches(/.+\@.+\..+/)
+      .withMessage("Email must contain @")
+      .isLength({ min: 4, max: 32 }),
+    check("password", "Password is required")
+      .not()
+      .isEmpty(),
+    check("password")
+      .isLength({ min: 6 })
+      .withMessage("Password must contain at least 6 characters")
+      .matches(/\d/)
+      .withMessage("Password must contain a number")
+  ],
+  signup
+);
 
 module.exports = router;
