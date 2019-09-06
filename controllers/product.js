77811b5dafcc503181f37835d2bf145722c62ff7
rakutenjongseok <whdtjr2792@gmail.com>
2019-09-07 05:26:07 +0900

    product controller update

--- controllers/product.js
+++ controllers/product.js
@@ -1,11 +1,12 @@
 const formidable = require("formidable");
-const _ = require("loadash");
+const _ = require("lodash");
 const fs = require("fs");
 const Product = require("../models/product");
 
 exports.create = (req, res) => {
   let form = new formidable.IncomingForm();
-  form.keepExtensions = trueform.parse(req, (err, fields, files) => {
+  form.keepExtensions = true;
+  form.parse(req, (err, fields, files) => {
     if (err) {
       return res.status(400).json({
         error: "Image could not be uploaded"
